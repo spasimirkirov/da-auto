da738003343e988558b4a28ea0405eb2fcf67c16
Spasimir Kirov <spasimirkirov@gmail.com>
2024-11-24 12:23:47 +0200

    Move Admin files to admin folders and implement base controllers for admin pages.

---
+++ app/Http/Controllers/Admin/SettingsController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Request;
+
+class SettingsController extends Controller
+{
+    public function index(Request $request)
+    {
+        return 1;
+    }
+
+    public function create(Request $request)
+    {
+        return 1;
+    }
+
+    public function store(FormRequest $request)
+    {
+        return 1;
+    }
+
+    public function edit(Request $request)
+    {
+        return 1;
+    }
+
+    public function update(FormRequest $request)
+    {
+        return 1;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,36 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ContactUsController;
 use App\Http\Controllers\FeaturedCarsController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->prefix('admin')->group(function () {
+    // Dashboard
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Users Management
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/edit', [UserController::class, 'create'])->name('users.edit');
+    Route::put('/users', [UserController::class, 'update'])->name('users.update');
+
+    // Cars
+    Route::get('/cars', [UserController::class, 'index'])->name('cars.index');
+    Route::get('/cars/create', [UserController::class, 'create'])->name('cars.create');
+    Route::post('/cars', [UserController::class, 'store'])->name('cars.store');
+    Route::get('/cars/edit', [UserController::class, 'create'])->name('cars.edit');
+    Route::put('/cars', [UserController::class, 'update'])->name('cars.update');
+
+    // Settings
+    Route::get('/settings', [UserController::class, 'index'])->name('settings.index');
+    Route::get('/settings/edit', [UserController::class, 'create'])->name('settings.edit');
+    Route::put('/settings', [UserController::class, 'update'])->name('settings.update');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -20,4 +42,4 @@ Route::get('/', [HomeController::class, 'index']);
 Route::get('/featured-cars', [FeaturedCarsController::class, 'index']);
 Route::get('/contact-us', [ContactUsController::class, 'index']);
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
