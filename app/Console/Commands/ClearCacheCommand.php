bfd4351d99a4ad25aa030c1415f5c89774226110
Spasimir Kirov <spasimirkirov@gmail.com>
2024-11-24 11:36:54 +0200

    Create command to be able to clear cache, config, view, routes at once.

---
+++ app/Console/Commands/ClearCacheCommand.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class ClearCacheCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'cc';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $this->call('cache:clear');
+        $this->call('config:clear');
+        $this->call('view:clear');
+        $this->call('route:clear');
+    }
+}
