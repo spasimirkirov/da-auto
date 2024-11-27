416ef9c41b907597bd919d95189ee5e83838921d
Spasimir Kirov <spasimirkirov@gmail.com>
2024-11-27 12:27:48 +0200

    Change title of admin panel

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -9,7 +9,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }} - {{ $pageTitle ?? 'Dashboard' }}</title>
+    <title>{{ config('app.name', 'Laravel') }} - {{ $pageTitle ?? 'Admin Panel' }}</title>
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss'])
